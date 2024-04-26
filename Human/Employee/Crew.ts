95c55a5535d7f53a3957e5c28f09575294938d8e
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-04-26 14:12:16 +0700

    change code in pilot #0

--- Human/Employee/Crew.ts
+++ Human/Employee/Crew.ts
@@ -1,14 +1,14 @@
-import { person } from "../Person";
+// import { Person } from "../Person";
 
-export enum crewMember{
-    PILOT,
-    CO_PILOT,
-    FLGHTATTENDENCE,
-}
+// export enum crewMember{
+//     PILOT,
+//     CO_PILOT,
+//     FLGHTATTENDENCE,
+// }
 
-export class Crew extends person{
-    private salary: number = 0; // by default
-    constructor(firstName:string, lastName:string){
-        super(firstName, lastName);
-    }
-}
+// export class Crew extends Person{
+//     private salary: number = 0; // by default
+//     constructor(firstName:string, lastName:string){
+//         super(firstName, lastName);
+//     }
+// }
--- Human/Passenger/Passenger.ts
+++ Human/Passenger/Passenger.ts
@@ -1,5 +1,4 @@
-import { Person } from "../Person";
-import { Gender } from "../Person";
+import { Gender,Person } from "../Person";
 export class Passenger extends Person {
     private passengerID: string;
     constructor(passengerID: string, firstName: string, lastName: string, email: string, phone: number, gender: Gender) {
