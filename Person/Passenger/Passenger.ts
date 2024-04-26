4cb93eccded1229ed04318cbeeb6464b38124736
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-04-26 10:54:35 +0700

    passenger attriute

--- Person/Passenger/Passenger.ts
+++ Person/Passenger/Passenger.ts
@@ -1,9 +1,9 @@
 import { Gender, Person } from "../Person";
 export class Passenger extends Person {
     private passengerID: string;
-    constructor(passengerID: string, firstName: string, lastName: string, email: string, phone: number) {
+    constructor(passengerID: string, firstName: string, lastName: string, email: string, phone: number, gender: Gender) {
         super(firstName, lastName, email, phone)
         this.passengerID = passengerID;
     }
-    getPassengerName() { return this.getName}
 }
+
