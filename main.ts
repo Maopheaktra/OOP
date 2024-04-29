f03bf63702d4762c01ccd1d4f24dd76c222953be
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-04-29 14:30:11 +0700

    src folder for each folders #0

--- main.ts
+++ main.ts
@@ -1,6 +1,2 @@
 // Import the employees the passenger
 
-// Import the departure and date-time 
-import { Departure } from "./src/Flight/Departure/Departure";
-import { date } from "./Date-time/date"
-import { time } from "./Date-time/time"
