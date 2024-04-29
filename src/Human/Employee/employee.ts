8d0331ca83cf4b05c3d2e3a2615eff13be65c651
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-04-29 16:40:27 +0700

    create file of the booking and employee #0

--- src/Human/Employee/employee.ts
+++ src/Human/Employee/employee.ts
@@ -1,12 +1,12 @@
-import { Person } from "../Person";
+import { Gender, Person } from "../Person";
 
 export abstract class Employee extends Person{
     private salary: number = 0; //by default
     private wordHour: number
     firstName : string
     lastName : string
-    constructor(salary: number, workHour: number, firstName: string, lastName: string, email: string, phone: number){
-        super(firstName, lastName, email, phone);
+    constructor(salary: number, workHour: number, firstName: string, lastName: string, email: string, phone: string, gender:Gender){
+        super(firstName, lastName, email, phone,gender);
         this.salary = salary;
         this.wordHour = workHour;
     }
