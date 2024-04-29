33c2deb1675bd54c5a750cf617d7ea3d22f40a85
RADY.DOEURN <RADY.DOEURN@PNC.LAN>
2024-04-29 20:40:30 +0700

    write code to create meal and baggages #12

--- Booking/booking.ts
+++ Booking/booking.ts
@@ -1,6 +1,9 @@
 import { Meal } from "./meal";
 import { Baggages } from "./bagages";
 import { Departure } from "../Departure/Departure";
+import { Passenger } from "../Person/Passenger/Passenger";
+
+
 
 export enum TypeTickets{
     return_Ticket,
@@ -11,16 +14,49 @@ export class Booking{
     private bookingID: string;
     private bookingReference: string;
     private bookingFlight: string;
-    private typeTickets: TypeTickets;
-    constructor(bookingID:string, bookingReference:string, bookingFlight:string, typeTickets:TypeTickets){
+    private passengers: Passenger;
+    private departurePlace: Departure;
+    private typeTickets: TypeTickets; 
+    private baggages: Baggages;
+    private meals: Meal;
+    bag: Baggages;
+
+    constructor(bookingID:string, bookingReference:string, bookingFlight:string, passengers: Passenger, departurePlace: Departure, typeTickets:TypeTickets, baggages: Baggages, meal: Meal){
         this.bookingID = bookingID
         this.bookingReference = bookingReference
         this.bookingFlight = bookingFlight
+        this.passengers = passengers
+        this.departurePlace = departurePlace
         this.typeTickets = typeTickets
+        this.baggages = baggages
+        this.meals = meal
+        
     }
+    
     getBookingID(){
         return this.bookingID;
     }
+
+    getPassenger(){
+        return this.passengers;
+    }
+
+    getReturnTicket(){
+        return this.typeTickets;
+    }
+
+    getMeal(meals: Meal){
+        return this.meals
+    }
+
+    setBaggage(bag:Baggages){
+        this.bag=bag
+    }
+
+    setTypeTickets(newTypeTickets:TypeTickets){
+        this.typeTickets = newTypeTickets;
+    }
+
 
 }
 
