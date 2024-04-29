a0cb700ecb4351dcacd824c2e04152afd5602987
CHORN.CHOAN <CHORN.CHOAN@PNC.LAN>
2024-04-29 14:14:17 +0700

    create fuldr src and instarl tsconfig #0

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // Import the employees the passenger
 
 // Import the departure and date-time 
-import { Departure } from "./Departure/Departure";
+import { Departure } from "./src/Flight/Departure/Departure";
 import { date } from "./Date-time/date"
 import { time } from "./Date-time/time"
