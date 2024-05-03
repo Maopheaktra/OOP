7fe6e261555da78946af46a41cb3f633b2f49f5b
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-05-03 20:23:19 +0700

    Update code for airline

--- src/Human/Employee/Co-Pilot.ts
+++ src/Human/Employee/Co-Pilot.ts
@@ -17,16 +17,28 @@ export class CoPilot extends Employee {
     ){
         super(Name,email,gender,Category);
         this.CardID = CardID;
-        this.date = date;
         this.Category = Category
     }
     addflight(flight: Flight){
         return this.flight.push(flight);
     }
     addSchedule(schedule: Date){
-        return this.date.push(schedule)
+        return this.date.push(schedule);
+    }
+    getSchedule(){
+        return this.date;
     }
     getPilotId(){
         return this.CardID;
     }
+    findPilotFlights(schedule: Date){
+        let listFlightForPilot =[]
+        for(let flight of this.flight){
+            if((flight.getFlightDate().getTime() == schedule.getTime()) && flight.getPilotFromFlight()){
+                listFlightForPilot.push(flight)
+            }
+          
+        }
+        return listFlightForPilot
+      }
 }
--- src/main.ts
+++ src/main.ts
@@ -17,37 +17,3 @@ import { Pilot } from "./Human/Employee/Pilot";
 import { CoPilot } from "./Human/Employee/Co-Pilot";
 import { time } from "./Date-time/time";
 
-// Create Departure----------
-let departure1 = new Departure("Camboia","PP");
-let arrival1 = new Departure("Camboia","Siem reap");
-// Create Airline+++++++++++++++
-let airline1 = new Airlines("PNC airline");
-// Create dae----------------
-let date1 = new Date("11/06/2024");
-//Create Time------------------
-let time1 = new time("23:00PM");
-let time2 = new time("23:00PM");
-// Create Seat+++++++++
-let seat1 = new Seat("12");
-let seat2 = new Seat("15");
-let seat3 = new Seat("126");
-let seat4 = new Seat("13");
-// Create Gate---------------
-let Gate1 = new Gate("1");
-let Gate2 = new Gate("2");
-
-// Create Route------------
-let route1= new Route(departure1, arrival1);
-
-// Create Pilot----------------
-let Pilot1 = new Pilot("1", "Chorn", "chorn@gmail.com", Gender.Male, crewMember.PILOT);
-
-// Create aeroplan+++++++++
-let aeroplan1 = new Aeroplanes("PHN");
-
-
-let Aeroplane1 = new Aeroplanes("PHN");
-let PM_Airline = new Airlines("PM_Airline");
-// Create passenger ---------------
-let passenger = new Passenger("003", "Pheaktra", "pheaktra@gmail.com",Gender.Female);
-// Add seat to aeroplan
